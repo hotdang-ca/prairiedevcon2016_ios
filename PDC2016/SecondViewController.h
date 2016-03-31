a77dd5bbeb324992fc693c3c530f42e2b4d3c6e0
James Perih <james.perih@tippingcanoe.com>
2016-03-31 10:20:50 -0500

    Initial Commit

---
+++ PDC2016/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  PDC2016
+//
+//  Created by James Perih on 2016-03-31.
+//  Copyright © 2016 Hot Dang Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ PDC2016/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDC2016
+//
+//  Created by James Perih on 2016-03-31.
+//  Copyright © 2016 Hot Dang Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
