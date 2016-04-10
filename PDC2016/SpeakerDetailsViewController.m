74653b714c62b2f589f98bcd20034e7c98fa5083
James Perih <james@hotdang.ca>
2016-04-10 14:44:36 -0500

    Add Edit for Speakers

--- PDC2016/SpeakerDetailsViewController.m
+++ PDC2016/SpeakerDetailsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SpeakerDetailsViewController.h"
+#import "DetailNotesViewController.h"
 
 #import "Speaker.h"
 
@@ -75,6 +76,18 @@
         } else {
             _blogButton.enabled = NO;
         }
+        
+        
+        UIImage *editImage = [UIImage imageNamed:@"edit"];
+        UIBarButtonItem *editButton = [[UIBarButtonItem alloc] bk_initWithImage:editImage style:UIBarButtonItemStylePlain handler:^(id sender) {
+            DetailNotesViewController *notesViewController = [[DetailNotesViewController alloc] initWithNibName:NSStringFromClass(DetailNotesViewController.class)  bundle:[NSBundle mainBundle]];
+            if (notesViewController) {
+                notesViewController.sessionOrSpeakerObject = _speaker;
+                [self.navigationController pushViewController:notesViewController animated:YES];
+            }
+        }];
+        
+        self.navigationItem.rightBarButtonItems = @[editButton];
     }
 }
 - (void)didReceiveMemoryWarning {
