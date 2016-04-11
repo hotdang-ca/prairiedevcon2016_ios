e0a7eeb7a2c002e429de2d409bd616f070062f39
James Perih <james@hotdang.ca>
2016-04-11 00:47:49 -0500

    #$@&%*! NSCoding/NSCopying

--- PDC2016/Speaker.m
+++ PDC2016/Speaker.m
@@ -25,4 +25,54 @@
                                                       }];
     return speakerMapping;
 }
+
+#pragma mark - NSCoding
+- (id)initWithCoder:(NSCoder *)decoder {
+    self = [super init];
+    if (self) {
+        self.identifier = [decoder decodeObjectForKey:@"identifier"];
+        self.name = [decoder decodeObjectForKey:@"name"];
+        self.companyName = [decoder decodeObjectForKey:@"companyName"];
+        self.city = [decoder decodeObjectForKey:@"city"];
+        self.region = [decoder decodeObjectForKey:@"region"];
+        self.bio = [decoder decodeObjectForKey:@"bio"];
+        self.imageUrlString = [decoder decodeObjectForKey:@"imageUrlString"];
+        self.blogUrlString = [decoder decodeObjectForKey:@"blogUrlString"];
+        self.webUrlString = [decoder decodeObjectForKey:@"webUrlString"];
+        self.twitterName = [decoder decodeObjectForKey:@"twitterName"];
+        self.sessions = [decoder decodeObjectForKey:@"sessions"];
+    }
+    return self;
+}
+- (void)encodeWithCoder:(NSCoder *)encoder {
+    [encoder encodeObject:self.identifier forKey:@"identifier"];
+    [encoder encodeObject:self.name forKey:@"name"];
+    [encoder encodeObject:self.companyName forKey:@"companyName"];
+    [encoder encodeObject:self.city forKey:@"city"];
+    [encoder encodeObject:self.region forKey:@"region"];
+    [encoder encodeObject:self.bio forKey:@"bio"];
+    [encoder encodeObject:self.imageUrlString forKey:@"imageUrlString"];
+    [encoder encodeObject:self.blogUrlString forKey:@"blogUrlString"];
+    [encoder encodeObject:self.webUrlString forKey:@"webUrlString"];
+    [encoder encodeObject:self.twitterName forKey:@"twitterName"];
+    [encoder encodeObject:self.sessions forKey:@"sessions"];
+}
+#pragma mark - NSCopying
+-(id)copyWithZone:(NSZone *)zone {
+    id copy = [[[self class] alloc] init];
+    if (copy) {
+        [copy setIdentifier:[self.identifier copyWithZone:zone]];
+        [copy setName:[self.name copyWithZone:zone]];
+        [copy setCompanyName:[self.companyName copyWithZone:zone]];
+        [copy setCity:[self.city copyWithZone:zone]];
+        [copy setRegion:[self.region copyWithZone:zone]];
+        [copy setBio:[self.bio copyWithZone:zone]];
+        [copy setImageUrlString:[self.imageUrlString copyWithZone:zone]];
+        [copy setBlogUrlString:[self.blogUrlString copyWithZone:zone]];
+        [copy setWebUrlString:[self.webUrlString copyWithZone:zone]];
+        [copy setTwitterName:[self.twitterName copyWithZone:zone]];
+        [copy setSessions:[self.sessions copyWithZone:zone]];
+    }
+    return copy;
+}
 @end
